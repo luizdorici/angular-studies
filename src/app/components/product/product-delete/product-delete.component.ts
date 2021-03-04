d79e2c872c95089bde31c3f29a27a576b39d137e
Luiz <luizdorici@gmail.com>
2021-03-04 16:17:40 -0300

    delete de produtos funcionando e adicao de errorHandler

--- src/app/components/product/product-delete/product-delete.component.ts
+++ src/app/components/product/product-delete/product-delete.component.ts
@@ -1,14 +1,14 @@
-import { Product } from './../product.model';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ProductService } from './../../../components/product/product.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
-  selector: 'app-product-update',
-  templateUrl: './product-update.component.html',
-  styleUrls: ['./product-update.component.css']
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
 })
-export class ProductUpdateComponent implements OnInit {
+export class ProductDeleteComponent implements OnInit {
 
   product!: Product
 
@@ -21,9 +21,9 @@ export class ProductUpdateComponent implements OnInit {
     });
   }
 
-  updateProduct(): void {
-    this.productService.update(this.product).subscribe(() => {
-      this.productService.showMessage('Produto atualizado com sucesso!')
+  deleteProduct(): void {
+    this.productService.delete(this.product.id).subscribe(() => {
+      this.productService.showMessage('Produto excluido com sucesso!')
       this.router.navigate(['/products'])
     })
   }
@@ -31,4 +31,5 @@ export class ProductUpdateComponent implements OnInit {
   cancel(): void {
     this.router.navigate(['/products'])
   }
+
 }
