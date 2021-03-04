24547fded72d7b4f55ae03bdef0bf6bea0897a11
Luiz <luizdorici@gmail.com>
2021-03-04 09:04:22 -0300

    criacao do product model e product service

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,6 @@
+import { ProductService } from './../../../components/product/product.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
@@ -9,9 +11,18 @@ export class ProductCreateComponent implements OnInit {
 
   //propLegal = "qualquer"
 
-  constructor() { }
+  constructor(private productService: ProductService,
+      private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  createProduct(): void {
+    this.productService.showMessage('Produto Criado!')
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
   }
 
   /*fazerAlgo(): void {
